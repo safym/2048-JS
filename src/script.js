4e986596f0480e01ec45773a15d60484e2a2fb7d
safym <osafym@gmail.com>
2022-09-01 05:55:53 +0300

    initial commit

---
+++ src/script.js
@@ -0,0 +1,16 @@
+var game = {
+    currentData:   [[0, 0, 0, 0],
+                    [0, 0, 0, 0],
+                    [0, 0, 0, 0],
+                    [0, 0, 0, 0]]
+}
+
+game.setNewNumber = function() {
+    console.log("setNewNumber");
+}
+
+game.setStartNumbers = function() {
+    console.log("setNewNumber");
+}
+
+game.setNewNumber();
